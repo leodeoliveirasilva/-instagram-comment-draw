2926d9c59e757bb2654f65791e2d0507165771f8
leodeoliveirasilva <leonardo.oliveirasilva1005@gmail.com>
2020-06-03 16:32:45 -0300

    get comments and media from user

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const Instagram = require('instagram-web-api')
 const { INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD } = process.env
  
 const client = new Instagram({ username: INSTAGRAM_USERNAME, password: INSTAGRAM_PASSWORD })
- 
-;(async () => {
+
+;(async () => {  
   await client.login()
-  const profile = await client.getProfile()
- 
-  console.log(profile)
+  const photos = await client.getPhotosByUsername({ username: 'joaodoveneno' })
+  const mediaComments = await client.getMediaComments({ shortcode: 'CA37wGmhAPV', first: '49', after: '' })
+  const commentsFormatted = mediaComments.edges && mediaComments.edges.map(e => {
+    return { text: e.node.text, username: e.node.owner && e.node.owner.username }
+  })
+  console.log(JSON.stringify(commentsFormatted));
+  console.log(JSON.stringify(photos));
 })()
